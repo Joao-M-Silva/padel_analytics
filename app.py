a945d8badbf03d1870e3f8df6110e636ed0289f0
Joao Silva <jsilvawasd@hotmail.com>
2024-09-21 11:30:40 +0100

    stuff

--- app.py
+++ app.py
@@ -1,3 +1,166 @@
+import json
+import numpy as np
 import streamlit as st
+import plotly.graph_objects as go
+
+from analytics import DataAnalytics
+from visualizations.padel_court import padel_court_2d
+
+
+FPS = 25
+
+if "df" not in st.session_state:
+    st.session_state["df"] = None
+
+with st.sidebar:
+    with st.form("data-form"):
+
+        path = st.text_input("Data path")
+        FPS = st.number_input("Video FPS", 25)
+        submitted = st.form_submit_button("Submit")
+    
+    if submitted:
+        with open(path) as f:
+            data = json.load(f)
+        
+        data_analytics = DataAnalytics.from_dict(data)
+        df = data_analytics.into_dataframe(FPS)
+        st.session_state["df"] = df
 
 st.title("Padel Analytics")
+
+if st.session_state["df"] is not None:
+    st.header("Loaded dataframe")
+    st.write("First 20 lines")
+    st.dataframe(st.session_state["df"].head(20))
+    st.markdown(f"- Number of lines: {len(st.session_state["df"])}")
+    st.write("- Columns: ")
+    st.write(st.session_state["df"].columns)
+
+    st.write("Player velocity as a function of time")
+    velocity_type_choice = st.radio(
+        "Type", 
+        ["Horizontal", "Vertical", "Absolute"],
+    )
+    velocity_type_mapper = {
+        "Horizontal": "x",
+        "Vertical": "y",
+        "Absolute": "norm",
+    }
+    velocity_type = velocity_type_mapper[velocity_type_choice]
+    fig = go.Figure()
+    padel_court = padel_court_2d()
+    for player_id in (1, 2, 3, 4):
+        fig.add_trace(
+            go.Scatter(
+                x=st.session_state["df"]["time"], 
+                y=np.abs(
+                    st.session_state["df"][
+                        f"player{player_id}_V{velocity_type}4"
+                    ].to_numpy()
+                ),
+                mode='lines',
+                name=f'Player {player_id}',
+            ),
+        )
+        st.write(f"Player {player_id}")
+        st.write(
+            "- Total distance (m): ",
+            st.session_state["df"][
+                f"player{player_id}_distance"
+            ].sum()
+        )
+        st.write(
+            " - Mean Velocity (m/s): ", 
+            st.session_state["df"][
+                f"player{player_id}_V{velocity_type}4"
+            ].abs().mean(),
+        )
+        st.write(
+            "- Maximum Velocity (m/s): ", 
+            st.session_state["df"][
+                f"player{player_id}_V{velocity_type}4"
+            ].abs().max(),
+        )
+
+    st.plotly_chart(fig)
+
+
+
+    
+    
+    player_choice = st.radio("Player: ", options=[1, 2, 3, 4])
+    min_value = st.session_state["df"][
+        f"player{player_choice}_V{velocity_type}4"
+    ].abs().min()
+    max_value = st.session_state["df"][
+        f"player{player_choice}_V{velocity_type}4"
+    ].abs().max()
+    velocity_interval = st.slider(
+        "Velocity Interval",
+        min_value, 
+        max_value,
+        (min_value, max_value),
+    )
+    st.session_state["df"]["QUERY_VELOCITY"] = st.session_state["df"][
+        f"player{player_choice}_V{velocity_type}4"
+    ].abs()
+    min_choice = velocity_interval[0]
+    max_choice = velocity_interval[1]
+    df_scatter = st.session_state["df"].query(
+        "@min_choice <= QUERY_VELOCITY <= @max_choice"
+    )
+        
+    padel_court.add_trace(
+        go.Scatter(
+            x=df_scatter[f"player{player_choice}_x"],
+            y=df_scatter[f"player{player_choice}_y"] * -1,
+            mode="markers",
+            name=f"Player {player_choice}",
+            text=df_scatter[
+                f"player{player_choice}_V{velocity_type}4"
+            ].abs(),
+            marker=dict(
+                color=df_scatter[
+                        f"player{player_choice}_V{velocity_type}4"
+                ].abs(),
+                size=12,
+                showscale=True,
+                colorscale="jet",
+            )
+        )
+    )
+
+    st.plotly_chart(padel_court)
+
+    padel_court = padel_court_2d()
+    time_span = st.slider(
+        "Time Interval",
+        0.0, 
+        st.session_state["df"]["time"].max(),
+    )
+    df_time = st.session_state["df"].query(
+        "time >= @time_span"
+    )
+    padel_court.add_trace(
+        go.Scatter(
+            x=df_time[f"player{player_choice}_x"],
+            y=df_time[f"player{player_choice}_y"] * -1,
+            mode="markers",
+            name=f"Player {player_choice}",
+            text=df_time[
+                f"player{player_choice}_V{velocity_type}4"
+            ].abs(),
+            marker=dict(
+                color=df_time[
+                        f"player{player_choice}_V{velocity_type}4"
+                ].abs(),
+                size=12,
+                showscale=True,
+                colorscale="jet",
+            )
+        )
+    )
+    st.plotly_chart(padel_court)
+
+
--- trackers/keypoints_tracker/keypoints_tracker.py
+++ trackers/keypoints_tracker/keypoints_tracker.py
@@ -59,11 +59,14 @@ class KeypointsTracker:
 
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     NUMBER_KEYPOINTS = 12
+    NUMBER_EXTRA_KEYPOINTS = 6
     
     def __init__(
         self, 
         model_path: str,
-        model_type: Literal["resnet", "yolo"] = "resnet"
+        extra_model_path: str = None,
+        model_type: Literal["resnet", "yolo"] = "resnet",
+        fixed_keypoints_detection: list[Keypoint] = None,
     ):
         self.model_type = model_type
         if model_type == "resnet":
@@ -79,6 +82,13 @@ class KeypointsTracker:
             self.model = YOLO(model_path)
         else:
             raise ValueError("Unknown model type")
+        
+        if extra_model_path:
+            self.extra_model = YOLO(extra_model_path)
+        else:
+            self.extra_model = None
+
+        self.fixed_keypoints_detection = fixed_keypoints_detection
 
     def parse_detections(
         self, 
@@ -118,25 +128,39 @@ class KeypointsTracker:
 
         return keypoints_detections
     
-    def detect_frame(self, frame: np.ndarray) -> list[Keypoint]:
+    def detect_frame(
+        self, 
+        frame: np.ndarray,
+        use_extra_model: bool = False,
+    ) -> list[Keypoint]:
 
         if self.model_type != "yolo":
             raise ValueError("Only YOLO can predict frame by frame")
 
-        point_mapper = {
-            0: 10,
-            1: 11,
-            2: 1,
-            3: 0,
-            4: 7,
-            5: 9,
-            6: 8,
-            7: 5,
-            8: 6,
-            9: 2,
-            10: 4,
-            11: 3,
-        }
+        if use_extra_model:
+            point_mapper = {
+                0: 0,
+                1: 1,
+                2: 2, 
+                3: 3,
+                4: 4, 
+                5: 5,
+            }
+        else:
+            point_mapper = {
+                0: 10,
+                1: 11,
+                2: 1,
+                3: 0,
+                4: 7,
+                5: 9,
+                6: 8,
+                7: 5,
+                8: 6,
+                9: 2,
+                10: 4,
+                11: 3,
+            }
 
         h_frame, w_frame = frame.shape[:2] 
         train_imgsz = 640
@@ -144,14 +168,25 @@ class KeypointsTracker:
         ratio_y = h_frame / train_imgsz
 
         keypoints = []
-        result = self.model.predict(
-            Image.fromarray(frame).resize((train_imgsz, train_imgsz)),
-            conf=0.5,
-            iou=0.7,
-            imgsz=train_imgsz,
-            device=self.DEVICE,
-            max_det=self.NUMBER_KEYPOINTS,
-        )[0]
+
+        if use_extra_model:
+            result = self.extra_model.predict(
+                Image.fromarray(frame).resize((train_imgsz, train_imgsz)),
+                conf=0.5,
+                iou=0.7,
+                imgsz=train_imgsz,
+                device=self.DEVICE,
+                max_det=self.NUMBER_EXTRA_KEYPOINTS,
+            )[0]
+        else:
+            result = self.model.predict(
+                Image.fromarray(frame).resize((train_imgsz, train_imgsz)),
+                conf=0.5,
+                iou=0.7,
+                imgsz=train_imgsz,
+                device=self.DEVICE,
+                max_det=self.NUMBER_KEYPOINTS,
+            )[0]
 
         for i, keypoint in enumerate(result.keypoints.xy.squeeze(0)):
             keypoints.append(
@@ -170,61 +205,76 @@ class KeypointsTracker:
         save_path: str | Path = None,
         load_path: str | Path = None,
         frequency: int = 1,
+        use_extra_model: bool = False,
     ) -> list[list[Keypoint]]:
         
-        if load_path is not None:
-            keypoints_detections = self.load_detections(load_path)
+        if self.fixed_keypoints_detection is not None:
+            keypoints_detections = [
+                self.fixed_keypoints_detection
+                for _ in range(len(frames))
+            ]
+            print("-"*20)
+            print("USING FIXED KEYPOINTS DETECTION")
+            print("-"*20)
+        else:
+            if load_path is not None:
+                keypoints_detections = self.load_detections(load_path)
             
-            return keypoints_detections
-        
-        self.model.to(self.DEVICE)
+                return keypoints_detections
+            
+            self.model.to(self.DEVICE)
+            if use_extra_model:
+                self.extra_model.to(self.DEVICE)
 
-        if self.model_type == "resnet":
-            self.model.eval()
-            dataset = InferenceKeypointDataset(frames)
-            loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+            if self.model_type == "resnet":
+                self.model.eval()
+                dataset = InferenceKeypointDataset(frames)
+                loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-            h_frame, w_frame = frames[0].shape[:2]
+                h_frame, w_frame = frames[0].shape[:2]
 
-            keypoints_detections = []
-            for batch in tqdm(loader):
-                with torch.no_grad():
-                    outputs = self.model(batch["image"].to(self.DEVICE))
-                    outputs = torch.nn.Sigmoid()(outputs).cpu().detach().numpy()
-                
-                for keypoints_detection in outputs:
-                    keypoints_detections.append(
-                        [
-                            Keypoint(
-                                i,
-                                (
-                                    keypoint[0] * w_frame,
-                                    keypoint[1] * h_frame,
+                keypoints_detections = []
+                for batch in tqdm(loader):
+                    with torch.no_grad():
+                        outputs = self.model(batch["image"].to(self.DEVICE))
+                        outputs = torch.nn.Sigmoid()(outputs).cpu().detach().numpy()
+                    
+                    for keypoints_detection in outputs:
+                        keypoints_detections.append(
+                            [
+                                Keypoint(
+                                    i,
+                                    (
+                                        keypoint[0] * w_frame,
+                                        keypoint[1] * h_frame,
+                                    )
                                 )
-                            )
-                            for i, keypoint in enumerate(
-                                keypoints_detection.reshape(
-                                    self.NUMBER_KEYPOINTS, 
-                                    2,
+                                for i, keypoint in enumerate(
+                                    keypoints_detection.reshape(
+                                        self.NUMBER_KEYPOINTS, 
+                                        2,
+                                    )
                                 )
-                            )
-                        ]
+                            ]
+                        )
+
+            elif self.model_type == "yolo":
+                keypoints_detections = []
+                for i in range(0, len(frames), frequency):
+                    keypoints = self.detect_frame(
+                        frames[i], 
+                        use_extra_model=use_extra_model,
                     )
+                    keypoints_detections += [
+                        keypoints 
+                        for _ in range(frequency)
+                    ]
+                
+                keypoints_detections = keypoints_detections[:len(frames)]
 
-        elif self.model_type == "yolo":
-            keypoints_detections = []
-            for i in range(0, len(frames), frequency):
-                keypoints = self.detect_frame(frames[i])
-                keypoints_detections += [
-                    keypoints 
-                    for _ in range(frequency)
-                ]
-            
-            keypoints_detections = keypoints_detections[:len(frames)]
-
-            #for frame in frames:
-            #    keypoints = self.detect_frame(frame)
-            #    keypoints_detections.append(keypoints)
+                #for frame in frames:
+                #    keypoints = self.detect_frame(frame)
+                #    keypoints_detections.append(keypoints)
 
         if save_path is not None and load_path is None:
 
@@ -239,6 +289,8 @@ class KeypointsTracker:
             print("Done.")
 
         self.model.to("cpu")
+        if use_extra_model:
+            self.extra_model.to("cpu")
 
         return keypoints_detections
 
