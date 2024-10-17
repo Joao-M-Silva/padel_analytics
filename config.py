0c4c4ef1eff9c3802d2171bcfc03b6a21b8505c0
Joao Silva <jsilvawasd@hotmail.com>
2024-10-17 08:29:24 +0100

    feat: add example video

--- config.py
+++ config.py
@@ -19,14 +19,14 @@ FIXED_COURT_KEYPOINTS_LOAD_PATH = "./cache/fixed_keypoints_detection.json"
 FIXED_COURT_KEYPOINTS_SAVE_PATH = None # "./cache/fixed_keypoints_detection.json"
 
 # Players tracker
-PLAYERS_TRACKER_MODEL = "./yolov8m.pt"
+PLAYERS_TRACKER_MODEL = "./weights/players_detection/yolov8m.pt"
 PLAYERS_TRACKER_BATCH_SIZE = 8
 PLAYERS_TRACKER_ANNOTATOR = "rectangle_bounding_box"
 PLAYERS_TRACKER_LOAD_PATH = "./cache/players_detections.json"
 PLAYERS_TRACKER_SAVE_PATH = "./cache/players_detections.json"
 
 # Players keypoints tracker
-PLAYERS_KEYPOINTS_TRACKER_MODEL = "./runs/pose/train3/weights/best.pt"
+PLAYERS_KEYPOINTS_TRACKER_MODEL = "./weights/players_keypoints_detection/best.pt"
 PLAYERS_KEYPOINTS_TRACKER_TRAIN_IMAGE_SIZE = 1280
 PLAYERS_KEYPOINTS_TRACKER_BATCH_SIZE = 8
 PLAYERS_KEYPOINTS_TRACKER_LOAD_PATH = "./cache/players_keypoints_detections.json"
@@ -41,7 +41,7 @@ BALL_TRACKER_LOAD_PATH = "./cache/ball_detections.json"
 BALL_TRACKER_SAVE_PATH = "./cache/ball_detections.json"
 
 # Court keypoints tracker
-KEYPOINTS_TRACKER_MODEL = "./runs/keypoints/train2/weights/best.pt"
+KEYPOINTS_TRACKER_MODEL = "./court_keypoints_detection/weights/best.pt"
 KEYPOINTS_TRACKER_BATCH_SIZE = 8
 KEYPOINTS_TRACKER_MODEL_TYPE = "yolo"
 KEYPOINTS_TRACKER_LOAD_PATH = None
