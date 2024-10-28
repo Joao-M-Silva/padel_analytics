21de38cdccadb5719d4c7efd103239da37526604
Alex Monras <amb.physis@gmail.com>
2024-10-28 15:39:18 +0100

    add test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,10 +27,18 @@ def court_keypoints() -> Keypoints:
 
 @fixture
 def ball_detections():
-    with open(Path(__file__).parent / "resources/ball_detections.json", "r") as f:
+    with open(Path(__file__).parent / "resources/full_ball_detections.json", "r") as f:
         ball_detections = json.load(f)
 
-    yield ball_detections[:39]
+    yield ball_detections
+
+
+@fixture
+def ballistic_detections():
+    with open(Path(__file__).parent / "resources/ballistic_detections1.json", "r") as f:
+        ballistic_detections = json.load(f)
+
+    yield ballistic_detections
 
 
 @fixture
